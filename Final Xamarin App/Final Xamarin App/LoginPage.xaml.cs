2142e4054ca869a5be7709d922bbae5f1ddf73ee
Rajput616 <saurabhrajput616@gmail.com>
2021-10-04 10:38:43 +0530

    Add project files.

---
+++ Final Xamarin App/Final Xamarin App/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Final_Xamarin_App
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void loginbtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignupPage());
+        }
+
+        async private void signup_btn_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignupPage());
+        }
+    }
+}
